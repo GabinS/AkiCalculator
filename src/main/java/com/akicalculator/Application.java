cf0c039e0416ef322e95b3946d2d740f85b067fa
GabinS <gabz98.gs@gmail.com>
2020-10-21 10:40:25 +0200

    #8 update menu oranisation

--- src/main/java/com/akicalculator/Application.java
+++ src/main/java/com/akicalculator/Application.java
@@ -1,74 +1,18 @@
 package com.akicalculator;
 
-import java.util.Scanner;
-
-import com.akicalculator.models.Addition;
+import com.akicalculator.utils.NavigationUtils;
 
 public class Application {
 	
-	public static final Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
         while (true) {
         	System.out.flush();
-        	printMenu();
-            selectMenu();
+        	NavigationUtils.printMenu();
+        	NavigationUtils.selectMenu();
 		}
     } 
     
-    /**
-	 * Display menu in console
-	 */
-	public static void printMenu () {
-	    System.out.println("|      AkiCalculator       |");
-	    System.out.println("| Opération:               |");
-	    System.out.println("|    [1] Addition          |");
-	    System.out.println("|[E] Exit                  |");
-	}
-	
-	
-	/**
-	 * Select action in menu
-	 */
-	public static void selectMenu() {
-		String menuSelected = null;
-		Boolean validMenu = false;
-		while(!validMenu) {
-		    System.out.println("Choix du menu : ");
-		    menuSelected = scan.nextLine();
-		    validMenu = true;
-		    
-		    switch (menuSelected) {
-			case "1":
-				System.out.println("Addition");
-				execAddition();
-				break;
-			case "E":
-				System.exit(0);
-				break;
-
-			default:
-				System.out.println("Choix du menu invalide !");
-				validMenu = false;
-				break;
-			}
-		    
-		}
-	}
-
-	public static void execAddition() {
-		float a = 0;
-		float b = 0;
-		System.out.println("a = ");
-		a = scan.nextFloat();
-		System.out.println("b = ");
-		b = scan.nextFloat();
-		
-		Addition add = new Addition(a);
-		float result = add.apply(b);
-		
-		System.out.println(String.valueOf(a) + " + " + String.valueOf(b) + " = " + String.valueOf(result));
-		
-	}
+    
 	
 }
