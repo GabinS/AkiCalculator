db1b31728aca68c6b4cd406873b71db6f8640337
chockella <jorisguerrier@gmail.com>
2020-10-22 15:24:17 +0200

    feature-28 SquareRoot function + tests

---
+++ src/main/java/com/akicalculator/models/SquareRoot.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package com.akicalculator.models;
+
+/**
+ * Square Root
+ *
+ */
+public class SquareRoot extends Operation{
+
+    public SquareRoot(float firstValue) {
+        super(firstValue, 0, "V");
+    }
+
+    /**
+     * Apply squareroot
+     * @return the squareroot result
+     */
+    public float apply() {
+        this.result = (float) Math.sqrt(this.firstValue);
+        return this.result;
+    }
+
+}
