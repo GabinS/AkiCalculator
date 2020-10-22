aba5dff4e54d0dc0c3e2a2b0a68ab0fb51be3e2d
emiliego <e.gougeon@iia-laval.fr>
2020-10-22 15:38:14 +0200

    Fix checkstyle errors

--- src/main/java/com/akicalculator/models/SquareRoot.java
+++ src/main/java/com/akicalculator/models/SquareRoot.java
@@ -1,26 +1,21 @@
-/**
- *
- */
 package com.akicalculator.models;
 
 /**
  * Square Root
- *
  */
-public class SquareRoot extends Operation{
-    
-     /**
+public class SquareRoot extends Operation {
+
+    /**
      * Default Constructor.
      * @param firstValue of operation
-     * @param secondValue of operation
      */
     public SquareRoot(float firstValue) {
         super(firstValue, 0, "V");
     }
 
     /**
-     * Apply squareroot
-     * @return the squareroot result
+     * Apply square root.
+     * @return the square root result
      */
     public float apply() {
         this.result = (float) Math.sqrt(this.firstValue) ;
